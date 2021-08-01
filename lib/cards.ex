a6f7c1d0eb20ee718220170cbf650f52388fe25e
Wellington de Lima <wellington.map@hotmail.com>
2021-08-01 11:10:30 -0300

    create load function

--- lib/cards.ex
+++ lib/cards.ex
@@ -19,4 +19,16 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exists"
+    end
+  end
 end
